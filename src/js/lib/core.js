824c223fde7059a5d57bb658532cd73ff76a7ba8
Andrey Borbot <broman3113@gmail.com>
2021-07-29 11:21:41 +0300

    Added shortcut "$" for querySelectorAll and functions show, hide, toggle

---
+++ src/js/lib/core.js
@@ -0,0 +1,17 @@
+const $ = function (selector) {
+    return new $.prototype.init(selector);
+}
+$.prototype.init = function (selector) {
+    if (!selector) {
+        return this; // {} Empty object
+    }
+    Object.assign(this, document.querySelectorAll(selector));
+    this.length = document.querySelectorAll(selector).length;
+    return this;
+};
+
+$.prototype.init.prototype = $.prototype;
+
+window.$ = $;
+
+export default $;
