65008cf9228c5b4bd7f408a9835dca5fc3ed7caf
Andrey Borbot <broman3113@gmail.com>
2021-07-29 14:04:35 +0300

    Added modules "Actions" and "Classes"

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -5,6 +5,13 @@ $.prototype.init = function (selector) {
     if (!selector) {
         return this; // {} Empty object
     }
+
+    if (selector.tagName) {
+        this[0] = selector;
+        this.length = 1;
+        return this;
+    }
+
     Object.assign(this, document.querySelectorAll(selector));
     this.length = document.querySelectorAll(selector).length;
     return this;
