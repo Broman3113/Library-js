5e1ddb5f8578f1051352e0ef15fe3b67625ba678
Andrey Borbot <broman3113@gmail.com>
2021-08-02 10:22:20 +0300

    Added effect animations FadeIn and FadeOut

---
+++ src/js/lib/modules/effects.js
@@ -0,0 +1,57 @@
+import $ from '../core';
+
+$.prototype.animateOverTime = function (dur, cb, fin) {
+    let timeStart;
+
+    function _animateOverTime(time) {
+        if (!timeStart) {
+            timeStart = time;
+        }
+        let timeElapsed = time - timeStart;
+        let completion = Math.min(timeElapsed / dur, 1);
+
+        cb(completion);
+
+        if (timeElapsed < dur) {
+            requestAnimationFrame(_animateOverTime);
+        } else {
+            if (typeof fin === 'function') {
+                fin();
+            }
+        }
+    }
+
+    return _animateOverTime;
+};
+
+$.prototype.fadeIn = function (dur, display = "block", fin) {
+    for (let i = 0; i < this.length; i++) {
+        this[i].style.display = display;
+
+        const _fadeIn = (completion) => {
+            this[i].style.opacity = completion;
+        };
+
+        const anim = this.animateOverTime(dur, _fadeIn, fin);
+        requestAnimationFrame(anim);
+    }
+    return this;
+};
+
+$.prototype.fadeOut = function (dur, fin) {
+    for (let i = 0; i < this.length; i++) {
+
+        const _fadeOut = (completion) => {
+            this[i].style.opacity = 1 - completion;
+            if (completion === 1) {
+                this[i].style.display = 'none'
+            }
+        };
+
+        const anim = this.animateOverTime(dur, _fadeOut, fin);
+        requestAnimationFrame(anim);
+    }
+    return this;
+};
+
+
