5939e3e6251cac09d7d91aa4f66c961843f15820
Andrey Borbot <broman3113@gmail.com>
2021-08-06 14:26:02 +0300

    Added static and dynamic slider render. Added possibility to configure dynamic slider.

--- dist/script.js
+++ dist/script.js
@@ -117,6 +117,156 @@ Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.accordion-head').accordi
 
 /***/ }),
 
+/***/ "./src/js/lib/components/carousel.js":
+/*!*******************************************!*\
+  !*** ./src/js/lib/components/carousel.js ***!
+  \*******************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.carousel = function () {
+  for (let i = 0; i < this.length; i++) {
+    const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width;
+    const slides = this[i].querySelectorAll('.carousel-item');
+    const slidesField = this[i].querySelector('.carousel-slides');
+    const dots = this[i].querySelectorAll('.carousel-indicators li');
+    slidesField.style.width = 100 * slides.length + '%';
+    slides.forEach(slide => {
+      slide.style.width = width;
+    });
+    let offset = 0;
+    let slideIndex = 0;
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i].querySelector('[data-slide="next"]')).click(e => {
+      e.preventDefault();
+
+      if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) {
+        offset = 0;
+      } else {
+        offset += +width.replace(/\D/g, '');
+      }
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slideIndex == slides.length - 1) {
+        slideIndex = 0;
+      } else {
+        slideIndex++;
+      }
+
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[slideIndex].classList.add('active');
+    });
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i].querySelector('[data-slide="prev"]')).click(e => {
+      e.preventDefault();
+
+      if (offset == 0) {
+        offset = +width.replace(/\D/g, '') * (slides.length - 1);
+      } else {
+        offset -= +width.replace(/\D/g, '');
+      }
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slideIndex == 0) {
+        slideIndex = slides.length - 1;
+      } else {
+        slideIndex--;
+      }
+
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[slideIndex].classList.add('active');
+    });
+    const sliderId = this[i].getAttribute('id');
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(`#${sliderId} .carousel-indicators li`).click(e => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+      slideIndex = slideTo;
+      offset = +width.replace(/\D/g, '') * slideTo;
+      slidesField.style.transform = `translateX(-${offset}px)`;
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[slideIndex].classList.add('active');
+    });
+  }
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.createCarousel = function ({
+  id = 'e',
+  settings
+} = {}) {
+  for (let i = 0; i < this.length; i++) {
+    let carousel = document.createElement('div');
+    carousel.classList.add('carousel');
+    carousel.id = id;
+    let images = [];
+    let dots = [];
+
+    for (let j = 0; j < settings.images.length; j++) {
+      let carouselItem = document.createElement('div');
+      carouselItem.innerHTML = `
+                <div class="carousel-item">
+                    <img src="${settings.images[j]}" alt="Slide ${j}">
+                </div>`;
+      images.push(carouselItem);
+      let carouselDot = document.createElement('li');
+      carouselDot.setAttribute('data-slide-to', `${j}`);
+
+      if (j == 0) {
+        carouselDot.classList.add('active');
+      }
+
+      dots.push(carouselDot);
+    }
+
+    carousel.innerHTML = `
+        <ol class="carousel-indicators">
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-slides">
+
+            </div>
+        </div>
+        <a href="#" class="carousel-prev" data-slide="prev">
+            <span class="carousel-prev-icon">&lt;</span>
+        </a>
+        <a href="#" class="carousel-next" data-slide="next">
+            <span class="carousel-prev-icon">&gt;</span>
+        </a>`;
+    carousel.querySelector('.carousel-indicators').append(...dots);
+    carousel.querySelector('.carousel-slides').append(...images);
+    this[i].appendChild(carousel);
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.carousel').carousel();
+    let focus = false;
+
+    function activateAnimation() {
+      focus = setInterval(function () {
+        carousel.querySelector('.carousel-next').click();
+      }, settings.autoSlideSpeed || 5000);
+    }
+
+    if (settings.autoSlide) {
+      activateAnimation();
+      [...carousel.querySelectorAll('.carousel-item'), carousel.querySelector('a.carousel-prev'), carousel.querySelector('.carousel-next')].forEach(item => {
+        item.addEventListener('mouseover', () => {
+          clearInterval(focus);
+        });
+      });
+      [...carousel.querySelectorAll('.carousel-item'), carousel.querySelector('a.carousel-prev'), carousel.querySelector('.carousel-next')].forEach(item => {
+        item.addEventListener('mouseleave', () => {
+          activateAnimation(focus);
+        });
+      });
+    }
+  }
+};
+
+Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.carousel').carousel();
+
+/***/ }),
+
 /***/ "./src/js/lib/components/dropdown.js":
 /*!*******************************************!*\
   !*** ./src/js/lib/components/dropdown.js ***!
@@ -338,6 +488,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_modal__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/modal */ "./src/js/lib/components/modal.js");
 /* harmony import */ var _components_tab__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./components/tab */ "./src/js/lib/components/tab.js");
 /* harmony import */ var _components_accordion__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./components/accordion */ "./src/js/lib/components/accordion.js");
+/* harmony import */ var _components_carousel__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./components/carousel */ "./src/js/lib/components/carousel.js");
+
 
 
 
@@ -773,6 +925,15 @@ Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('#trigger').click(() =>
     }]]
   }
 }));
+Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.lol').createCarousel({
+  id: 'Hi',
+  settings: {
+    autoSlide: true,
+    autoSlideSpeed: 2000,
+    // Default value 5000
+    images: ['https://external-preview.redd.it/ODWbknJyIknx7ltFAMt-_TMgSDFHqoKXCLMrJR5sYOU.jpg?auto=webp&s=8e961e6afbc8984f0b0e0592adbd12da0e47a543', 'https://i.pinimg.com/originals/d2/cf/ed/d2cfed3d0eb1b5a543744d8c5f1bd396.jpg', 'https://p4.wallpaperbetter.com/wallpaper/405/544/857/treasure-planet-disney-clouds-face-stars-hd-wallpaper-preview.jpg', 'https://c4.wallpaperflare.com/wallpaper/980/308/988/treasure-planet-disney-space-ship-boat-hd-wallpaper-preview.jpg', 'https://static.wikia.nocookie.net/disney/images/2/2d/Treasure-planet-disneyscreencaps.com-233.jpg/revision/latest?cb=20130403024637']
+  }
+});
 
 /***/ })
 
